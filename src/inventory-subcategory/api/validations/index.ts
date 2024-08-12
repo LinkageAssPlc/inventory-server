ea053fc9437598fe95f7566d8ec81e108937ef42
LinkageAssPlc <itdev@linkageassurance.com>
2024-08-12 18:35:23 +0100

    sub category validation fixed

--- src/inventory-subcategory/api/validations/index.ts
+++ src/inventory-subcategory/api/validations/index.ts
@@ -1,10 +1,11 @@
 import { Joi } from "celebrate";
-
+import { toObjectId } from "../../../inventory-shared/validateAndConvertToObjectID";
 
 export default{
     subCategory: {
         body: Joi.object({
             name: Joi.string().min(2).max(24).required(),
+            categoryID: Joi.string().min(24).max(24).custom(toObjectId),
         })
     }
 }
